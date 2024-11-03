0abad3c4c8a74e58a87e312a103abd5d5105b1aa
Fajar Abdi Nugraha <fajar.nugraha@xtremax.com>
2024-11-03 22:31:44 +0700

    add(util): common/basic utility

---
+++ backend/pkg/util/env.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
+func GetEnvOrPanic(key string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	panic(fmt.Sprintf("Environment variable %s is not set", key))
+}
+
+func GetEnvAsInt(key string, fallback int) int {
+	if value, ok := os.LookupEnv(key); ok {
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			return fallback
+		}
+
+		return i
+	}
+
+	return fallback
+}
+
+func GetEnvAsBool(key string, fallback bool) bool {
+	if value, ok := os.LookupEnv(key); ok {
+		b, err := strconv.ParseBool(value)
+		if err != nil {
+			return fallback
+		}
+
+		return b
+	}
+
+	return fallback
+}
