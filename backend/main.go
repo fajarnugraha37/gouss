be3c23325d94efe3ac73bf0c471dee2216ba417c
Fajar Abdi Nugraha <fajar.nugraha@xtremax.com>
2024-11-03 23:49:39 +0700

    fix(routing): svelte routing when serve in go

--- backend/main.go
+++ backend/main.go
@@ -2,23 +2,52 @@ package main
 
 import (
 	"log"
-    "strconv"
+	"strconv"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/rewrite"
 	"github.com/gofiber/fiber/v3/middleware/static"
-	
+
 	"github.com/fajarnugraha37/gouss/config"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Use("/favicon.png", static.New("./assets/public/favicon.png"))
-	app.Use("/_app", static.New("./assets/public/_app", static.ConfigDefault, static.Config{Compress: true}))
-	app.Use([]string{"/", "/index.html"}, static.New("./assets/public/index.html"))
-	app.Get("/hello-world", func(c fiber.Ctx) error {
+	// static assets
+	app.Get("/", static.New("./assets/public/index.html"))
+	app.Use([]string{"/favicon.png", "/favicon.ico"}, static.New("./assets/public/favicon.png"))
+	app.Use("_app", static.New("./assets/public/_app", static.Config{ Compress: true }))
+	
+	// api
+	apiGroup := app.Group("/api")
+	apiGroup.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
+
+	app.Use(
+		[]string{
+			"/*.html", "/*.htm",
+			"/*.ico", "/*.png", "/*.webp", 
+			"/*.js", "/*.css", 
+			"/*.json", "/*.xml",
+		}, 
+		static.New("./assets/public", static.Config{ Compress: true }),
+	)
+	app.Use(
+		"/*",
+		rewrite.New(rewrite.Config{
+			Rules: map[string]string{"/*": "/$1.html"},
+			Next: func(c fiber.Ctx) bool {
+				if c.Is("json") || c.Route().Method != "GET" {
+					return true
+				}
+
+				return false
+			},
+		}),
+		static.New("./assets/public", static.Config{Compress: true}),
+	)
 
 	address := config.AppProperty.App.Host + ":" + strconv.Itoa(config.AppProperty.App.Port)
 	log.Fatal(app.Listen(address))
